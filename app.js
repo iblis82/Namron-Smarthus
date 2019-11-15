266f1df959e87e31b001a8587fa5693e0b9eceb9
maginawin <maginawin@163.com>
2019-11-15 20:05:32 +0800

    Add some Jason's ZG devices,
    next, go on...

--- app.js
+++ app.js
@@ -2,12 +2,32 @@
 
 const Homey = require('homey');
 
-class MyApp extends Homey.App {
+class EasyHome extends Homey.App {
 	
 	onInit() {
-		this.log('MyApp is running...');
+
+		this.log('EasyHome is running...');
+
+		//Z-button scene trigger cards
+		this.triggerWallController_scene = new Homey.FlowCardTriggerDevice('wall_controller_scene');
+		this.triggerWallController_scene
+			.register()
+			.registerRunListener((args, state) =>
+			 Promise.resolve(args.button.id === state.button && args.scene.id === state.scene));
+
+		this.triggerWallController_scene
+			.getArgument('scene')
+			.registerAutocompleteListener((query, args, callback) => args.device.onSceneAutocomplete(query, args, callback));
+		this.triggerWallController_scene
+			.getArgument('button')
+			.registerAutocompleteListener((query, args, callback) => args.device.onButtonAutocomplete(query, args, callback));
+
+		//Z-button button trigger cards
+		this.triggerWallController_button = new Homey.FlowCardTriggerDevice('wall_controller_button');
+		this.triggerWallController_button
+			.register();
 	}
 	
 }
 
-module.exports = MyApp;
+module.exports = EasyHome;
